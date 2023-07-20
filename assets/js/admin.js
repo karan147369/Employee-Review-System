422237d79c685d2f3ed657f3ccb5667a0c437e81
karan147369 <103363950+karan147369@users.noreply.github.com>
2023-07-20 18:15:21 +0530

    gitignore

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,15 +1,1 @@
-let empList = [];
-const pageNumber = 1;
-const limit = 5;
-fetch(`http://localhost:4000/getEmpList/${pageNumber}/${limit}`)
-  .then((response) => response.json())
-  .then((response) => {
-    empList = response.data;
-    console.log(empList);
-    // return res.render("adminPage.ejs", { empList: empList, limit: 5 });
-    window.location.reload();
-  })
-  .catch((err) => {
-    console.error("Error in calling api: " + err);
-    return res.render("home.ejs");
-  });
+
